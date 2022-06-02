7d503d334cf4b08b0d7152c4d72ef91aa4904e70
arefin6 <arefinhossain3@gmail.com>
2022-06-02 12:47:26 +0600

    woking on upvote api

---
+++ pages/api/favouriteCoffeStoreById.js
@@ -0,0 +1,41 @@
+import {  findRecordByFilter, table} from "../../libs/airTable";
+const favouriteCoffeeStoreById = async(req,res) =>{
+
+   if(req.method === "PUT"){
+       try {
+           const {id} = req.body;
+           if(id){
+               const records = await findRecordByFilter(id);
+               if(records.length !== 0){
+                   const record = records[0];
+                   const calculateVoting = parseInt(record.vote)+1;
+                   
+                   const updateRecord = await table.update([{
+                       id:record.id,
+                       fields:{
+                          vote:calculateVoting 
+                       },
+                    }]);
+                     if(updateRecord){
+                        res.json({records})
+                     }
+               }
+               else{
+                res.status(401)
+                res.json({message:"coffeeStore Doesn't Exits",id})  
+               } 
+           }
+           else{
+            res.status(400)
+            res.json({message:"Id is missing"})  
+           }
+       } catch (error) {
+          console.error("Error UpVoting",error);
+          res.status(500)
+          res.json({message:"Error UpVoting"}) 
+       }
+   }
+
+}
+
+export default favouriteCoffeeStoreById;
